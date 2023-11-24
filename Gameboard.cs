33dc739658d6ffeb49afaab93de4d3bd6bd6fa63
jpang18 <jpang18@bi.kgapupils.uk>
2023-11-24 16:10:48 +0000

    AddtestProgram

--- Gameboard.cs
+++ Gameboard.cs
@@ -2,90 +2,124 @@ using System;
 namespace HelloWorld { 
 class Gameboard { 
     int[,] board = new int[10,10];
-    Ship[] ships;
+    Ship[] ships = new Ship[5];
     public int[,] Board {get{return board;}set{board = value;}}
     public Ship[] Ships {get{return ships;}set{ships = value;}}
     public Gameboard(){
-        int len = board.Length;
+        int len = board.GetLength(0);
         for (int i = 0;i<len;i++){
             for (int j = 0;j<len;j++){
                 board[i,j] = 0;
             }   
         }
-        ships[0] = new Ship(0,2);
-        ships[1] = new Ship(0,3);
-        ships[2] = new Ship(0,3);
-        ships[3] = new Ship(0,4);
-        ships[4] = new Ship(0,5);
+        Ships[0] = new Ship(2,0);
+        Ships[1] = new Ship(3,0);
+        Ships[2] = new Ship(3,0);
+        Ships[3] = new Ship(4,0);
+        Ships[4] = new Ship(5,0);
     }
     // n is the length of the ship
     public void Place(int n){
+        Console.WriteLine($"Ship{n}");
+        Console.WriteLine("Hint: The x, y cooridinates are for the head of the ship");
         // x is the coordinate of the gameboard
+        Console.Write($"Enter x:");
         int x = Program.CheckInput(Console.ReadLine());
-        // x is the coordinate of the gameboard
+        // y is the coordinate of the gameboard
+        Console.Write($"Enter y:");
         int y = Program.CheckInput(Console.ReadLine());
-        board = PlaceShip(n,x,y);
+        // d is the direction of the ship   
+        Console.WriteLine("1:Vertical 2:Horizontal");
+        Console.Write($"Enter direction:");
+        string d = PlaceDirection(Console.ReadLine());
+        board = PlaceShip(n,x,y,d);
     }
-    public bool ValidShip(int n, int x, int y){
+    public bool ValidShip(int n, int x, int y,string d){
         bool valid = false;
         int count = 0;
-        for (int i = 0;i<n;i++){
-            if (board[x,y+i] != 1 || board[x+i,y] != 1){
+        if (d == "1"){
+            for (int i = 0;i<n;i++){
+            if (board[x,y+i] != 1){
                 count++;
             }
         }
-        if (count == 5){
+        }else{
+            for (int i = 0;i<n;i++){
+            if (board[x+i,y] != 1){
+                count++;
+            }
+        }
+        }
+        if (count == n){
             valid = true;
         }
         return valid;
     }
-    string PlaceDirection(){
-        string d = "";
+    string PlaceDirection(string d){
         bool success = false;
         while (success == false){
-            Console.WriteLine("1:Vertical 2:Horizontal");
-            d = Console.ReadLine();
             if (d == "1" || d == "2"){
                 success = true;
             }else{
                 Console.WriteLine("Wrong Direction");
+                Console.WriteLine("1:Vertical 2:Horizontal");
+                Console.Write($"Enter direction:");
+                d = Console.ReadLine();
             }
         }
         return d;
     }
-    public int[,] PlaceShip(int n, int x, int y){
-        Ship s = ships[n];
+    public int[,] PlaceShip(int n, int x, int y,string d){
+        Ship s = Ships[n];
         int len = s.Length;
-        if (len+y > 9 || len+x > 9 || ValidShip(n,x,y) == false){
+        if (len+y > 9 || len+x > 9 || ValidShip(n,x,y,d) == false){
             Console.WriteLine("Cannot put ship there");
             Console.WriteLine("Enter x,y again");
             Place(n);
         }else{
-            if (PlaceDirection() == "1"){
+            if (d == "1"){
                 for (int i = 0;i<len;i++){
                     board[x+i,y] = 1;
                     s.X[i] = x+i;
                     s.Y[i] = y;
+                }
+            }else{
+                for (int i = 0;i<len;i++){
+                    board[x,y+i] = 1;
+                    s.X[i] = x;
+                    s.Y[i] = y+i;
             }
             }
         }
         return board;
     }
     public void DisplayBoard(){
-        int len = board.Length;
+        int len = board.GetLength(0);
+        for (int x = -1;x<len;x++){
+            Console.Write($"{x}".PadRight(4));
+        }
+        Console.Write("\n");
         for (int i = 0;i<len;i++){
+            Console.Write($"{i}".PadRight(4));    
             for (int j = 0;j<len;j++){
-                Console.WriteLine($"{board[i,j]}".PadRight(10));
-            }            
+                Console.Write($"{board[i,j]}".PadRight(4));
+            }   
+            Console.Write("\n");      
         }
     }
     public void DisplayStatus(){
         int len = ships.Length;
-        for (int i = 0; i<len;i++){
-            Console.WriteLine($"Ship{i}");
-            Console.WriteLine($"Length{ships[i].Length}");
-            Console.WriteLine($"Damaged:{ships[i].Damaged}");
-            Console.WriteLine($"Sunk:{ships[i].Sunk}");
+        Console.Write($"Ship".PadRight(10));
+        Console.Write($"Length".PadRight(10));
+        Console.Write($"Damaged".PadRight(10));
+        Console.Write($"Sunk".PadRight(10));
+        Console.WriteLine("");
+        for (int i=0;i<len;i++){
+            Console.Write($"{i+1}".PadRight(10));
+            Console.Write($"{Ships[i].Length}".PadRight(10));
+            Console.Write($"{Ships[i].Damaged}".PadRight(10));
+            Console.Write($"{Ships[i].Sunk}".PadRight(10));
+            Console.Write("\n");
         }
     }
 } 
--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
 ﻿using System;
 namespace HelloWorld { 
 class Program { 
-static void Main(string[] args)
+static void Main()
  { 
-Console.WriteLine("Hello, World!"); 
+    Console.WriteLine("Welcome to BattleShips");
+    Player player1 = new Player();
+    Player player2 = new Player();
+    player1.Name = "Jack";
+    player2.Name = "Don";
+    TestSetupPhrase(player1);
+    TestSetupPhrase(player2);
+    TestShPhrase(player1,player2);
+    TestShPhrase(player2,player1);
 } 
 public static int CheckInput(string x){
     bool success = false;
@@ -18,6 +26,71 @@ public static int CheckInput(string x){
         }
     }
     return y;
-}
 } 
+public static void SetupPhrase(Player player){
+    for (int i=0;i<5;i++){
+        // x is the coordinate of the gameboard
+        Console.Write($"Enter x:");
+        int x = CheckInput(Console.ReadLine());
+        // y is the coordinate of the gameboard
+        Console.Write($"Enter y:");
+        int y = CheckInput(Console.ReadLine());
+        player.Gboard.PlaceShip(i,x,y,"2");
+    }
+    Console.Write($"{player.Name}:\n");
+    Console.Write("x is Horizontal\n");
+    Console.WriteLine("y is Veritcal\n");
+    player.Gboard.DisplayBoard();
+}
+
+public static void ShPhrase(Player shooter, Player nshooter){
+    Console.WriteLine($"{shooter.Name} Shooting -> {nshooter.Name}");
+    // x is the coordinate of the gameboard
+    Console.Write($"Enter x:");
+    int x = CheckInput(Console.ReadLine());
+    // y is the coordinate of the gameboard
+    Console.Write($"Enter y:");
+    int y = CheckInput(Console.ReadLine());
+    shooter.Mboard[x,y] = 1;
+    shooter.MaskedBoard(shooter.Mboard);
+    DisplayOutcome(shooter.Shoot(nshooter,x,y));
+    shooter.CheckWin(shooter,nshooter);
+}
+static void DisplayOutcome(bool result){
+    if (result){
+        Console.WriteLine("Hitted");
+    }else{
+        Console.WriteLine("Nothing is hit");
+    }
+}
+
+public static void TestSetupPhrase(Player player){
+    for (int i = 0;i<5;i++){
+        player.Gboard.PlaceShip(i,i,i,"2");
+    }
+    Console.Write($"{player.Name}:\n");
+    Console.Write("x is Horizontal\n");
+    Console.WriteLine("y is Veritcal\n");
+    player.Gboard.DisplayBoard();
+}
+public static void TestShPhrase(Player shooter, Player nshooter){
+    int count = 0;
+    Console.WriteLine($"{shooter.Name} Shooting -> {nshooter.Name}");
+    for (int x=0;x<6;x++){
+        for (int y=0;y<6;y++){
+            shooter.Mboard[x,y] = 1;
+            if (shooter.Shoot(nshooter,x,y)==true){
+                count++;
+            }
+            shooter.CheckWin(shooter,nshooter);
+        }
+    }
+    shooter.MaskedBoard(shooter.Mboard);
+    Console.WriteLine($"{count}");
+    Console.WriteLine($"{nshooter.Name} Board:");
+    nshooter.Gboard.DisplayBoard();
+    Console.WriteLine($"{nshooter.Name} Ships:");
+    nshooter.Gboard.DisplayStatus();
+}
+}
 }
